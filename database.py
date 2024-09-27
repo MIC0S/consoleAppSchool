288e5240b030e6d2a6973ccdf9c32965097c3aa8
Micos <znik0007@gmail.com>
2024-09-27 16:53:09 +0300

    hw #2 - added login with pgdb and repeat command

---
+++ database.py
@@ -0,0 +1,49 @@
+import os
+import psycopg2
+import psycopg2.pool
+import hashlib
+
+
+class Database:
+    def __init__(self):
+        POSTGRES_USER = os.environ['POSTGRES_USER']
+        POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+        POSTGRES_DATABASE = os.environ['POSTGRES_DATABASE']
+        POSTGRES_HOST = os.environ['POSTGRES_HOST']
+        POSTGRES_PORT = os.environ['POSTGRES_PORT']
+        self.HASH_SALT = os.environ['HASH_SALT']
+        self.connection_pool = psycopg2.pool.SimpleConnectionPool(
+            1, 10,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD,
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            database=POSTGRES_DATABASE
+        )
+
+    def createUser(self, username, password):
+        password_hashed = hashlib.sha224((password + self.HASH_SALT).encode()).hexdigest()
+        conn = self.connection_pool.getconn()
+        status = 0
+        try:
+            cursor = conn.cursor()
+            cursor.execute(f'INSERT INTO users (username, password) VALUES(%s, %s);', (username, password_hashed))
+            conn.commit()
+        except:
+            status = -1
+        finally:
+            self.connection_pool.putconn(conn)
+        return status
+
+    def authUser(self, username, password):
+        password_hashed = hashlib.sha224((password + self.HASH_SALT).encode()).hexdigest()
+        conn = self.connection_pool.getconn()
+        try:
+            cursor = conn.cursor()
+            cursor.execute(f'SELECT password FROM users WHERE username=%s;', (username,))
+            results = cursor.fetchone()
+            if results is None:
+                return False
+            return password_hashed == results[0]
+        finally:
+            self.connection_pool.putconn(conn)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+from database import Database
 
+db = Database()
 
 def execute(command, arguments):
     if command == 'shutdown':
@@ -15,14 +17,61 @@ def execute(command, arguments):
         return 0
     if command == 'help':
         print(f"""List of avaliable commands:
-shutdown         - shuts down the program
-echo             - repeats all arguments back
-cls              - clears console
-help             - gives this list""")
+shutdown                 - shuts down the program
+echo [text]              - repeats all arguments back
+cls                      - clears console
+help                     - gives this list
+repeat [amount] [text]   - repeats [text] given [amount] of times
+adduser [name] [pwd]     - creates new user with [name] and [pwd] password""")
+        return 0
+    if command == 'repeat':
+        try:
+            amount = int(arguments[1])
+            text = " ".join(arguments[2:])
+            print(amount*text)
+        except:
+            print('Invalid args given!')
+        return 0
+    if command == 'adduser':
+        if len(arguments) == 3:
+            status = db.createUser(arguments[1], arguments[2])
+            if status == -1:
+                print('Username claimed')
+            else:
+                print('User added successfully!')
+        else:
+            print('wrong arguments!')
         return 0
 
 
+def auth():
+    used_attempts = 0
+    while True:
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+
+        if db.authUser(username, password):
+            return True
+        else:
+            used_attempts += 1
+            os.system('cls' if os.name == 'nt' else 'clear')
+            if used_attempts >= 3:
+                return False
+            else:
+                print(f'Incorrect Credentials, please try again:\nTotal attempts {used_attempts}/3')
+
+
+
+
+
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    authStatus = auth()
+    if not authStatus:
+        print('Auth failed')
+        return -1
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Welcome!')
     while True:
         command_list = input().split()
         command = None
@@ -33,7 +82,7 @@ def main():
         try:
             res = execute(command, command_list[0:])
             if res == -1:
-                return
+                return 0
         except BaseException as error:
             print(f'Error during code execution: {error}')
 
