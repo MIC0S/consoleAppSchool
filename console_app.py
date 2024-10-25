ea47ff6ee4cf167a2715c4264195eb7a3a296816
Micos <znik0007@gmail.com>
2024-10-25 23:00:59 +0300

    hw #5 - old project frozen, simple UI app

---
+++ console_app.py
@@ -0,0 +1,62 @@
+import os
+from database import Database
+import functions as f
+
+db = Database()
+
+
+def execute(command, args):
+    if command == 'shutdown':
+        return f.shutdown()
+    if command == 'echo':
+        return f.echo(args)
+    if command == 'cls':
+        return f.cls()
+    if command == 'help':
+        return f.helpExt()
+    if command == 'repeat':
+        return f.repeat(args)
+    if command == 'adduser':
+        return f.adduser(args)
+
+
+def auth():
+    used_attempts = 0
+    while True:
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+
+        if db.authUser(username, password):
+            return username
+        else:
+            used_attempts += 1
+            os.system('cls' if os.name == 'nt' else 'clear')
+            if used_attempts >= 3:
+                return False
+            else:
+                print(f'Incorrect Credentials, please try again:\nTotal attempts {used_attempts}/3')
+
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    authStatus = auth()
+    if not authStatus:
+        print('Auth failed')
+        return -1
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'Welcome, dear {authStatus}')
+
+    while True:
+        print(authStatus, end="> ")
+        command_list = input().split()
+        command = None
+        if len(command_list) > 0:
+            command = command_list[0].lower()
+        else:
+            pass
+        try:
+            res = execute(command, command_list[0:])
+            if res == -1:
+                return 0
+        except BaseException as error:
+            print(f'Error during code execution: {error}')
---
+++ ui.py
@@ -0,0 +1,73 @@
+import sys
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QToolBar, QLabel, QStatusBar)
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtCore import QTimer, Qt
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Жук Никита Сергеевич №8")
+        self.counter = 0
+
+        self.counter_label = QLabel(f"Current Score: {self.counter}", self)
+        self.counter_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(self.counter_label)
+
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+        menubar = self.menuBar()
+        menu = None
+        for i in range(3):
+            menu = QMenu(f"Add {i * 3 + 1} - {i * 3 + 3}", self)
+            menubar.addMenu(menu)
+            for j in range(1, 4):
+                action = QAction(f"Add {i * 3 + j}", self)
+                action.triggered.connect(lambda checked, jl=j, il=i: self.changeCounter(il * 3 + jl))
+                menu.addAction(action)
+
+        exit_action = QAction("Exit", self)
+        exit_action.triggered.connect(self.close)
+        menu.addAction(exit_action)
+
+        toolbar = QToolBar("Main Toolbar")
+        icon_paths = [
+            QIcon.fromTheme("document-new"),
+            QIcon.fromTheme("document-open"),
+            # QIcon.fromTheme("document-save"),
+            # QIcon.fromTheme("document-properties")
+        ]
+        toolbar_action_name = [
+            'New Game', 'Open Game',  # 'Save Game', 'Settings'
+        ]
+
+        for i in range(len(icon_paths)):
+            action = QAction(icon_paths[i], toolbar_action_name[i], self)
+            toolbar.addAction(action)
+
+            action.triggered.connect(
+                lambda checked, display_time=i + 1, button_name=toolbar_action_name[i]:
+                    self.displayStatus(f"Action {button_name} executed!", display_time)
+            )
+
+        self.addToolBar(toolbar)
+
+    def changeCounter(self, amount):
+        self.counter += amount
+        self.counter_label.setText(f"Current Score: {self.counter}")
+
+    def displayStatus(self, text, time_to_display):
+        self.statusBar.showMessage(text, time_to_display * 1000)
+        QTimer.singleShot(time_to_display * 1000, lambda: self.statusBar.clearMessage())
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = MyWindow()
+    window.resize(1200, 900)
+    window.show()
+
+    sys.exit(app.exec())
